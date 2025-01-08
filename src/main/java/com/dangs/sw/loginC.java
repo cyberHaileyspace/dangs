c307db0f86f30e3c9e57bc26c74fcc4a378ad702
huengs99 <lastpang0123@gmail.com>
2025-01-08 15:39:13 +0900

    gitignore config

---
+++ src/main/java/com/dangs/sw/loginC.java
@@ -0,0 +1,21 @@
+package com.dangs.sw;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/loginC")
+public class loginC extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			swM.login
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+}
