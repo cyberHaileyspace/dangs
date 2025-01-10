60985753e699dec3f8eb211e4fbcf3282634fe6d
yj <cyoos1103@gmail.com>
2025-01-10 11:45:32 +0900

    .

--- src/main/java/com/dangs/sw/swM.java
+++ src/main/java/com/dangs/sw/swM.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.dangs.yj.DBManager;
+import com.dangs.main.DBManager;
 
 
 
