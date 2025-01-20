ead7a91778e0efbb750f046e231bed8a953d4eb4
yj <cyoos1103@gmail.com>
2025-01-20 11:12:14 +0900

    add js

--- src/main/webapp/js/yj/cm.js
+++ src/main/webapp/js/yj/cm.js
@@ -6,7 +6,7 @@ function searchCm(){
 	console.log(cmTitle);
 		
 		$.ajax({
-			url:'ReviewSearchC',	//어디로 요청?
+			url:'CmSearchC',	//어디로 요청?
 			data : {cmTitle}	// 파라미터
 		}).done(function(resData){
 			
@@ -29,3 +29,41 @@ function searchCm(){
 	})
 }
 
+
+
+
+
+
+function showResult(resData){
+	$.each(resData, function(i, r){
+		console.log(i);
+		console.log(r);
+		
+//		백틱!
+//<fmt:formatDate value="${r.r_date }"/>
+		let content = `
+      <div class="post-cm" onclick="location.href='CmPostC?no=${i.cm_no }'">
+      	<div class="cm-kind">
+      		<div class="cm-menu">${i.cm_cate }</div>
+      		<div class="cm-cate">${i.cm_size }</div>
+      	</div>
+      <div class="cm-title">${i.cm_title }</div>
+      <div class="cm-content">
+      <div class="cm-text"><span>${i.cm_text }</span></div>
+      <div class="cm-img"><img alt="" src="img/dog-nose.png"></div>
+      </div>
+      <div class="cm-info">
+      	<div class="info-name">작성자 : ${i.cm_user_id }</div>
+      	<div class="info-date">작성일 : ${i.cm_date }</div>
+      	<div class="info-view">조회수 : ${i.cm_view }</div>
+      	<div class="info-like">좋아요 : ${i.cm_like }</div>
+      	<%-- <div hidden>${i_cm_no }</div> --%>
+      </div>
+<!--       <hr> -->
+      </div>
+      </c:forEach>`;
+		
+		$("#cm-result").append(content);
+		
+	})
+}
